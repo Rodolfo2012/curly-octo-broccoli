c07a3408de1da57436f373045215467c803e42a0
bob sabixão <joserodolfo304510@gmail.com>
2021-03-25 08:35:16 -0300

    meu primeiro versionamento

---
+++ exercicio 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+
+
+
+namespace exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string N1  = Console.ReadLine();
+           int N3 = int.Parse(Console.ReadLine());
+           float N4 = float.Parse(Console.ReadLine());
+           string N6 = (Console.ReadLine());
+           
+            Console.WriteLine("Entre com o seu nome");
+            Console.WriteLine(N1);
+            Console.WriteLine("Quantos quartos tem a casa");
+            Console.WriteLine(N3);
+            Console.WriteLine("Entre com o preço do produto:");
+            Console.WriteLine(N4);
+            Console.WriteLine("Entre seu ultimo nome , idade e altura:");
+            Console.WriteLine(N6);
+            
+
+           
+
+
+          
+
+
+           
+        }
+    }
+}
